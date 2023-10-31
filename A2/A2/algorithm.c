f159cc821f4c5b70be33f82316d2ccd6f44376bb
Ubuntu <gustave.charles--saigne@epfl.ch>
2023-10-31 21:08:15 +0100

    algorithm finished

--- A2/A2/algorithm.c
+++ A2/A2/algorithm.c
@@ -11,24 +11,40 @@ SCIPER      : Your SCIPER numbers
 #define INPUT(I,J) input[(I)*length+(J)]
 #define OUTPUT(I,J) output[(I)*length+(J)]
 
+#define TILE_SIZE 8
+
 void simulate(double *input, double *output, int threads, int length, int iterations)
 {
     double *temp;
+    int i,j,i1,j1;
     
     // Parallelize this!!
+    omp_set_num_threads(threads);  // Set the number of threads
+
+   
     for(int n=0; n < iterations; n++)
     {
-        for(int i=1; i<length-1; i++)
-        {
-            for(int j=1; j<length-1; j++)
+        #pragma omp parallel for private(i, j, i1, j1) collapse(2)
+        for(i=1; i<length-1; i += TILE_SIZE)
+        { 
+            for(j=1; j<length-1; j += TILE_SIZE)
             {
-                    if ( ((i == length/2-1) || (i== length/2))
-                        && ((j == length/2-1) || (j == length/2)) )
-                        continue;
 
-                    OUTPUT(i,j) = (INPUT(i-1,j-1) + INPUT(i-1,j) + INPUT(i-1,j+1) +
-                                   INPUT(i,j-1)   + INPUT(i,j)   + INPUT(i,j+1)   +
-                                   INPUT(i+1,j-1) + INPUT(i+1,j) + INPUT(i+1,j+1) )/9;
+                        int i_end = i + TILE_SIZE < length ? i + TILE_SIZE : length-1;
+                        int j_end = j + TILE_SIZE < length ? j + TILE_SIZE : length-1;
+
+                        for (i1 = i; i1 < i_end; ++i1){
+                            for (j1 = j; j1 < j_end; ++j1){
+                                if ( ((i1 == length/2-1) || (i1== length/2))
+                                && ((j1 == length/2-1) || (j1 == length/2)) )
+                                continue;
+
+                                OUTPUT(i1,j1) = (INPUT(i1-1,j1-1) + INPUT(i1-1,j1) + INPUT(i1-1,j1+1) +
+                                       INPUT(i1,j1-1)   + INPUT(i1,j1)   + INPUT(i1,j1+1)   +
+                                       INPUT(i1+1,j1-1) + INPUT(i1+1,j1) + INPUT(i1+1,j1+1) )/9;
+                            }
+                        }
+
             }
         }
 
