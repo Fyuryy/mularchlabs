f36c1f4e6d0f069d9627d924be3b5dace7d42c5c
Fyury <pedro.laginhasgouveia@epfl.ch>
2023-12-12 15:19:30 +0100

    okay

--- A4/implementation.cu
+++ A4/implementation.cu
@@ -13,7 +13,7 @@ SCIPER      : Your SCIPER number
 using namespace std;
 
 // CPU Baseline
-void array_process(double *input, double *output, int length, int iterations)
+__global__ void array_process(double *input, double *output, int length, int iterations)
 {
     double *temp;
 
@@ -60,25 +60,47 @@ void GPU_array_process(double *input, double *output, int length, int iterations
     cudaEventCreate(&comp_end);
 
     /* Preprocessing goes here */
+    double *d_input, *d_output;
+    cudaMalloc((void**)&d_input, length*length*sizeof(double));
+    cudaMalloc((void**)&d_output, length*length*sizeof(double));
 
     cudaEventRecord(cpy_H2D_start);
     /* Copying array from host to device goes here */
+    cudaMemcpy(d_input, input, length, cudaMemcpyHostToDevice);
+
     cudaEventRecord(cpy_H2D_end);
     cudaEventSynchronize(cpy_H2D_end);
 
     //Copy array from host to device
     cudaEventRecord(comp_start);
     /* GPU calculation goes here */
+
+    dim3 dimBlock(16, 16);
+    dim3 dimGrid((length + dimBlock.x -1) / dimBlock.x, (length + dimBlock.y -1) / dimBlock.y);
+
+    for(int n = 0; n < iterations; n++){
+        array_process<<<dimGrid, dimBlock>>>(d_input, d_output, length, iterations);
+
+        double *temp = d_input;
+        d_input = d_output;
+        d_output = temp;
+    }
+
+
     cudaEventRecord(comp_end);
     cudaEventSynchronize(comp_end);
 
     cudaEventRecord(cpy_D2H_start);
     /* Copying array from device to host goes here */
+
+    cudaMemcpy(output, d_output, length, cudaMemcpyDeviceToHost);
+
     cudaEventRecord(cpy_D2H_end);
     cudaEventSynchronize(cpy_D2H_end);
 
     /* Postprocessing goes here */
-
+    cudaFree(d_input);
+    cudaFree(d_output);
     float time;
     cudaEventElapsedTime(&time, cpy_H2D_start, cpy_H2D_end);
     cout<<"Host to Device MemCpy takes "<<setprecision(4)<<time/1000<<"s"<<endl;
