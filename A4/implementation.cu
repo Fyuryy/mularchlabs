b53e5b68b05333c54bd06a489b16a89fee1a7f01
Ubuntu <gustave.charles--saigne@epfl.ch>
2023-12-21 09:11:03 +0100

    added kernel

--- A4/implementation.cu
+++ A4/implementation.cu
@@ -20,28 +20,28 @@ void array_process(double *input, double *output, int length, int iterations)
 {
     double *temp;
 
-    for(int n=0; n<(int) iterations; n++)
+    for (int n = 0; n < (int)iterations; n++)
     {
-        for(int i=1; i<length-1; i++)
+        for (int i = 1; i < length - 1; i++)
         {
-            for(int j=1; j<length-1; j++)
+            for (int j = 1; j < length - 1; j++)
             {
-                output[(i)*(length)+(j)] = (input[(i-1)*(length)+(j-1)] +
-                                            input[(i-1)*(length)+(j)]   +
-                                            input[(i-1)*(length)+(j+1)] +
-                                            input[(i)*(length)+(j-1)]   +
-                                            input[(i)*(length)+(j)]     +
-                                            input[(i)*(length)+(j+1)]   +
-                                            input[(i+1)*(length)+(j-1)] +
-                                            input[(i+1)*(length)+(j)]   +
-                                            input[(i+1)*(length)+(j+1)] ) / 9;
-
+                output[(i) * (length) + (j)] = (input[(i - 1) * (length) + (j - 1)] +
+                                                input[(i - 1) * (length) + (j)] +
+                                                input[(i - 1) * (length) + (j + 1)] +
+                                                input[(i) * (length) + (j - 1)] +
+                                                input[(i) * (length) + (j)] +
+                                                input[(i) * (length) + (j + 1)] +
+                                                input[(i + 1) * (length) + (j - 1)] +
+                                                input[(i + 1) * (length) + (j)] +
+                                                input[(i + 1) * (length) + (j + 1)]) /
+                                               9;
             }
         }
-        output[(length/2-1)*length+(length/2-1)] = 1000;
-        output[(length/2)*length+(length/2-1)]   = 1000;
-        output[(length/2-1)*length+(length/2)]   = 1000;
-        output[(length/2)*length+(length/2)]     = 1000;
+        output[(length / 2 - 1) * length + (length / 2 - 1)] = 1000;
+        output[(length / 2) * length + (length / 2 - 1)] = 1000;
+        output[(length / 2 - 1) * length + (length / 2)] = 1000;
+        output[(length / 2) * length + (length / 2)] = 1000;
 
         temp = input;
         input = output;
@@ -49,29 +49,19 @@ void array_process(double *input, double *output, int length, int iterations)
     }
 }
 
-
-
-
-_global_ kernel(double* input, double* output, size_t length){
+_global_ void kernel(double *input, double *output, size_t length)
+{
 
     int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
-    
 
-
-
+    int array_index = (j * ROW_SIZE) + i;
 }
-
-
-
-
-
-
 
 // GPU Optimized function
 void GPU_array_process(double *input, double *output, int length, int iterations)
 {
-    //Cuda events for calculating elapsed time
+    // Cuda events for calculating elapsed time
     cudaEvent_t cpy_H2D_start, cpy_H2D_end, comp_start, comp_end, cpy_D2H_start, cpy_D2H_end;
     cudaEventCreate(&cpy_H2D_start);
     cudaEventCreate(&cpy_H2D_end);
@@ -79,42 +69,36 @@ void GPU_array_process(double *input, double *output, int length, int iterations
     cudaEventCreate(&cpy_D2H_end);
     cudaEventCreate(&comp_start);
     cudaEventCreate(&comp_end);
-    /* Preprocessing goes here */
 
+    /* Preprocessing goes here */
     double *d_input, *d_output;
     size_t size = length * length * sizeof(double);
 
-
-    if(!cudaMalloc((void**)&d_input, size))
+    if (!cudaMalloc((void **)&d_input, size))
         return;
 
-    if(cudaMalloc((void**)&d_output, size))
+    if (cudaMalloc((void **)&d_output, size))
         cudaFree(d_input);
-        return;
-
-    
-
+    return;
 
     cudaEventRecord(cpy_H2D_start);
 
     /* Copying array from host to device goes here */
     if(!cudaMemcpy(d_input, input, length, cudaMemcpyHostToDevice));
-        cudaFree(d_input);
-        cudaFree(d_output);
-        return;
+
+    cudaFree(d_input);
+    cudaFree(d_output);
+    return;
 
     cudaEventRecord(cpy_H2D_end);
     cudaEventSynchronize(cpy_H2D_end);
-    //Copy array from host to device
+    // Copy array from host to device
     cudaEventRecord(comp_start);
     /* GPU calculation goes here */
-
-
-
-    //!must invoke our kernels here
-
-
-
+    dim3 thrsPerBlock(3, 4); // 3x4
+    dim3 nBlks(2, 3);        // 2x3
+    kernel<<<nBlks, thrsPerBlock>>>();
+    //! must invoke our kernels here
     cudaEventRecord(comp_end);
     cudaEventSynchronize(comp_end);
     cudaEventRecord(cpy_D2H_start);
@@ -127,9 +111,9 @@ void GPU_array_process(double *input, double *output, int length, int iterations
     cudaFree(d_output);
     float time;
     cudaEventElapsedTime(&time, cpy_H2D_start, cpy_H2D_end);
-    cout<<"Host to Device MemCpy takes "<<setprecision(4)<<time/1000<<"s"<<endl;
+    cout << "Host to Device MemCpy takes " << setprecision(4) << time / 1000 << "s" << endl;
     cudaEventElapsedTime(&time, comp_start, comp_end);
-    cout<<"Computation takes "<<setprecision(4)<<time/1000<<"s"<<endl;
+    cout << "Computation takes " << setprecision(4) << time / 1000 << "s" << endl;
     cudaEventElapsedTime(&time, cpy_D2H_start, cpy_D2H_end);
-    cout<<"Device to Host MemCpy takes "<<setprecision(4)<<time/1000<<"s"<<endl;
+    cout << "Device to Host MemCpy takes " << setprecision(4) << time / 1000 << "s" << endl;
 }
