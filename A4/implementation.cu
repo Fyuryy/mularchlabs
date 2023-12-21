d15ddca7d1f96190e7d0ee213c8f76b36a845dc1
Fyury <pedro.laginhasgouveia@epfl.ch>
2023-12-21 08:49:55 +0100

    last changes

--- A4/implementation.cu
+++ A4/implementation.cu
@@ -10,10 +10,13 @@ SCIPER      : Your SCIPER number
 #include <iomanip>
 #include <sys/time.h>
 #include <cuda_runtime.h>
+
+#define MAX_HEAT 1000
+
 using namespace std;
 
 // CPU Baseline
-__global__ void array_process(double *input, double *output, int length, int iterations)
+void array_process(double *input, double *output, int length, int iterations)
 {
     double *temp;
 
@@ -47,6 +50,24 @@ __global__ void array_process(double *input, double *output, int length, int ite
 }
 
 
+
+
+_global_ kernel(double* input, double* output, size_t length){
+
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    
+
+
+
+}
+
+
+
+
+
+
+
 // GPU Optimized function
 void GPU_array_process(double *input, double *output, int length, int iterations)
 {
@@ -58,56 +79,57 @@ void GPU_array_process(double *input, double *output, int length, int iterations
     cudaEventCreate(&cpy_D2H_end);
     cudaEventCreate(&comp_start);
     cudaEventCreate(&comp_end);
-
     /* Preprocessing goes here */
+
     double *d_input, *d_output;
-    cudaMalloc((void**)&d_input, length*length*sizeof(double));
-    cudaMalloc((void**)&d_output, length*length*sizeof(double));
+    size_t size = length * length * sizeof(double);
+
+
+    if(!cudaMalloc((void**)&d_input, size))
+        return;
+
+    if(cudaMalloc((void**)&d_output, size))
+        cudaFree(d_input);
+        return;
+
+    
+
 
     cudaEventRecord(cpy_H2D_start);
+
     /* Copying array from host to device goes here */
-    cudaMemcpy(d_input, input, length, cudaMemcpyHostToDevice);
+    if(!cudaMemcpy(d_input, input, length, cudaMemcpyHostToDevice));
+        cudaFree(d_input);
+        cudaFree(d_output);
+        return;
 
     cudaEventRecord(cpy_H2D_end);
     cudaEventSynchronize(cpy_H2D_end);
-
     //Copy array from host to device
     cudaEventRecord(comp_start);
     /* GPU calculation goes here */
 
-    dim3 dimBlock(16, 16);
-    dim3 dimGrid((length + dimBlock.x -1) / dimBlock.x, (length + dimBlock.y -1) / dimBlock.y);
 
-    for(int n = 0; n < iterations; n++){
-        array_process<<<dimGrid, dimBlock>>>(d_input, d_output, length, iterations);
 
-        double *temp = d_input;
-        d_input = d_output;
-        d_output = temp;
-    }
+    //!must invoke our kernels here
+
 
 
     cudaEventRecord(comp_end);
     cudaEventSynchronize(comp_end);
-
     cudaEventRecord(cpy_D2H_start);
     /* Copying array from device to host goes here */
-
     cudaMemcpy(output, d_output, length, cudaMemcpyDeviceToHost);
-
     cudaEventRecord(cpy_D2H_end);
     cudaEventSynchronize(cpy_D2H_end);
-
     /* Postprocessing goes here */
     cudaFree(d_input);
     cudaFree(d_output);
     float time;
     cudaEventElapsedTime(&time, cpy_H2D_start, cpy_H2D_end);
     cout<<"Host to Device MemCpy takes "<<setprecision(4)<<time/1000<<"s"<<endl;
-
     cudaEventElapsedTime(&time, comp_start, comp_end);
     cout<<"Computation takes "<<setprecision(4)<<time/1000<<"s"<<endl;
-
     cudaEventElapsedTime(&time, cpy_D2H_start, cpy_D2H_end);
     cout<<"Device to Host MemCpy takes "<<setprecision(4)<<time/1000<<"s"<<endl;
 }
