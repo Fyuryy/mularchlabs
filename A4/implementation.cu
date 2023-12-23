e0d5b6fef35494d8c04d06c24efdf4e7ec201eda
Fyury <pedro.laginhasgouveia@epfl.ch>
2023-12-23 18:57:23 +0100

    workable version for foque saque

--- A4/implementation.cu
+++ A4/implementation.cu
@@ -59,7 +59,11 @@ __global__ void kernel(double *input, double *output, size_t length)
 
     //*border threads can be ignored as borders are always 0.
 
-    output[index] = (input[(i - 1) * (length) + (j - 1)] +
+
+    if(i < length - 1 && j < length -1 && i > 0 && j > 0){
+
+    
+        output[index] = (input[(i - 1) * (length) + (j - 1)] +
                      input[(i - 1) * (length) + (j)] +
                      input[(i - 1) * (length) + (j + 1)] +
                      input[(i) * (length) + (j - 1)] +
@@ -70,10 +74,11 @@ __global__ void kernel(double *input, double *output, size_t length)
                      input[(i + 1) * (length) + (j + 1)]) /
                     9;
 
-    output[(length / 2 - 1) * length + (length / 2 - 1)] = 1000;
-    output[(length / 2) * length + (length / 2 - 1)] = 1000;
-    output[(length / 2 - 1) * length + (length / 2)] = 1000;
-    output[(length / 2) * length + (length / 2)] = 1000;
+        output[(length / 2 - 1) * length + (length / 2 - 1)] = MAX_HEAT;
+        output[(length / 2) * length + (length / 2 - 1)] = MAX_HEAT;
+        output[(length / 2 - 1) * length + (length / 2)] = MAX_HEAT;
+        output[(length / 2) * length + (length / 2)] = MAX_HEAT;
+    }
 }
 
 // GPU Optimized function
@@ -99,7 +104,7 @@ void GPU_array_process(double *input, double *output, int length, int iterations
     /* Copying array from host to device goes here */
     cudaEventRecord(cpy_H2D_start);
 
-    cudaMemcpy(d_input, input, size, cudaMemcpyHostToDevice);
+    cudaMemcpy((double*) d_input, (double*)input, size, cudaMemcpyHostToDevice);
 
     cudaEventRecord(cpy_H2D_end);
     cudaEventSynchronize(cpy_H2D_end);
@@ -111,14 +116,16 @@ void GPU_array_process(double *input, double *output, int length, int iterations
     // dim3 nbThreads(32, 32, 1);
 
     // We organize the thread blocks into 2D arrays of threads.
-    dim3 nbBlocks(ceil((double)length / 8), ceil((double)length / 8));
-    dim3 nbThreads(8, 8);
+
+    dim3 blockD(32, 32);
+    dim3 nbThreads( (length-2) / blockD.x + 1, (length-2)  / blockD.y + 1);
+
     double *temp;
     cudaEventRecord(comp_start);
 
     for (int i = 0; i < iterations; i++)
     {
-        kernel<<<nbBlocks, nbThreads>>>(d_input, d_output, length);
+        kernel<<<nbThreads, blockD>>>(d_input, d_output, length);
 
         temp = d_input;
         d_input = d_output;
